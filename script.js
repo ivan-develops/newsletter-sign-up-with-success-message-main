0485469faae499c24e5e6c57a46873f3e95c14b0
Iván Vargas O <ivanvargas.va@gmail.com>
2025-09-04 03:40:03 -0500

    feat: validate form

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
+const form = document.getElementById('form');
 const submit = document.getElementById('submit');
 const modal = document.querySelector('.modal');
-const modal__button = document.querySelector('.button__modal');
+const email__input = document.getElementById('email__input');
+const email__states = document.getElementById('email__states');
 
-// onsubmit form
-const form = document.getElementById('form');
-function handleSubmit(e) {
-  e.preventDefault() // prevent the default behaviour
+
+
+const showError = ( message )=> {
+    email__states.innerText = message;
+    email__states.classList.add('email__error-states');
+    email__input.classList.add('invalid');
 }
 
-form.addEventListener('submit', handleSubmit);
-//
+const isValidEmail = (email)=> {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
 
-const showModal = ()=> {
-    modal.classList.toggle('active');
+  if(email__input.value === '') {
+        showError('Email is required');        /* call function */
+    } else if (!isValidEmail(email__input.value)) {
+        showError('Email is not valid');
+    } else {
+        showSuccess(email__input.value , 'Avalible email');
+    }
+
+});
+
+const showSuccess = (emailVerified , message)=> {
+    modal.classList.add('active');
+    const modal__email = modal.querySelector('.modal__email');
+    modal__email.innerText = emailVerified;
+    email__input.classList.add('valid');
+    email__states.innerText = message;
+    email__states.classList.add('email__succes-states');
 }
 
 const hideModal = ()=> {
     modal.classList.toggle('active');
+    console.log('EJECUTADO! hideModal ');
 }
-
-submit.addEventListener( 'click' , showModal );
-modal__button.addEventListener( 'click' , hideModal );
+// const modal__button = document.querySelector('.button__modal button');
+const modal__button = document.getElementById('modal-closer');
+modal__button.addEventListener('click',hideModal);
+console.log(modal__button);
